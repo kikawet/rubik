3b3a2e01c67bef0462bc1341fbc90799994f8d0c
kikawet <lopezortegafrancisco1@gmail.com>
2025-08-28 13:49:26 +0200

    add performance testing for solver

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS=-std=c99 -Wall -Wextra -Wpedantic -I./raylib-5.5_linux_amd64/include
 # FIXME: right now the program links with libraylib.so because I want solver and program to share the same raylib instance.
 # The goal is to statically link solver + raylib && fix rubik.c to not load anything
 
-all: solver_tester
+all: rubik solver_perf
 
 rubik: rubik.c model.o draw.o libsolver.so
 	gcc $(CFLAGS) -L./raylib-5.5_linux_amd64/lib -o $@ rubik.c model.o draw.o -l:libraylib.so -lm -Wl,-rpath=./raylib-5.5_linux_amd64/lib
@@ -20,6 +20,9 @@ libsolver.so: beginner_solver.c solver.h model.o
 
 solver.o: beginner_solver.c solver.h
 	gcc $(CFLAGS) -o $@ -c $<
+
+solver_perf: solver_perf.c solver.o model.o
+	gcc $(CFLAGS) -L./raylib-5.5_linux_amd64/lib -ggdb -o $@ $^ -l:libraylib.a -lm
 
 clean:
 	rm rubik
---
+++ solver_perf.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <time.h>
+#include <raylib.h>
+
+#include "solver.h"
+
+bool is_face_solved(const Face* f, const EColor color)
+{
+    return
+        getCell(f, F_U | F_L) == color &&
+        getCell(f, F_U) == color &&
+        getCell(f, F_U | F_R) == color &&
+
+        getCell(f, F_L) == color &&
+        getCell(f, F_R) == color &&
+
+        getCell(f, F_D | F_L) == color &&
+        getCell(f, F_D) == color &&
+        getCell(f, F_D | F_R) == color;
+}
+
+bool is_solved(const Cube* cube)
+{
+    const Face* up = &cube->faces[f2i(F_U)];
+    const Face* down = &cube->faces[f2i(F_D)];
+    const Face* front = &cube->faces[f2i(F_F)];
+    const Face* back = &cube->faces[f2i(F_B)];
+    const Face* left = &cube->faces[f2i(F_L)];
+    const Face* right = &cube->faces[f2i(F_R)];
+
+    return
+        is_face_solved(up, f2c(F_U)) &&
+        is_face_solved(down, f2c(F_D)) &&
+        is_face_solved(front, f2c(F_F)) &&
+        is_face_solved(back, f2c(F_B)) &&
+        is_face_solved(left, f2c(F_L)) &&
+        is_face_solved(right, f2c(F_R));
+}
+
+void applyMoves(Cube* cube, const Moves* queue)
+{
+    for (size_t i = 0; i < queue->count; i++)
+        rotateCube(cube, queue->items[i]);
+}
+
+void dump_moves(const Moves* moves)
+{
+    for (size_t i = 0; i < moves->count; i++)
+        fprintf(stderr, "%s", moveToStr(moves->items[i]));
+
+    fprintf(stderr, "\n");
+}
+
+void parse_args(const int argc, const char** argv, Moves* moves)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s \"[moves]\"\n", argv[0]);
+    }
+
+    strToMoves(argv[1], moves);
+}
+
+int main(const int argc, const char** argv)
+{
+    SetTraceLogLevel(LOG_NONE);
+
+    Cube cube = newCube();
+    Moves shuffle = {0};
+    Moves solution = {0};
+
+    parse_args(argc, argv, &shuffle);
+    applyMoves(&cube, &shuffle);
+
+    solve(cube, &solution);
+    applyMoves(&cube, &solution);
+
+    if (!is_solved(&cube))
+    {
+        dump_moves(&shuffle);
+        abort();
+    }
+
+    if (shuffle.items) free(shuffle.items);
+    if (solution.items) free(solution.items);
+
+    return 0;
+}
